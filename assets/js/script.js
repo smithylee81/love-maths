bde7ad740c969a45bfdbb863ad0055ebcf5dd161
smithylee81 <smithylee81@yahoo.co.uk>
2021-11-12 13:35:28 +0000

    Update code following non save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 //Wait for the DOM to finish loeading before running the game
 //Get the button elements and add event listeners to them
 
@@ -11,20 +10,32 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
-        })
+        });
     }
-})
+
+    runGame("addition");
+
+});
 
 /** 
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
 */
 function runGame(){
+
     //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() *25) + 1;
     let num2 = Math.floor(Math.random() *25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer(){
@@ -43,7 +54,11 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
